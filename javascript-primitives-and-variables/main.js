f834ec4c56d4b9045bbc49339d94f1e3804b095b
Sam Vivonia <snvivonia@gmail.com>
2021-07-12 19:20:26 -0700

    Completed javascript-primitives-and-variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,11 @@
+var fullName = 'Sam Vivonia';
+var isCool = 'depends who you ask';
+var totalPets = 2;
+var unknown = null;
+var never;
+
+console.log('Full Name: ' + fullName + ', Type: ' + typeof fullName);
+console.log('Is Cool: ' + isCool + ', Type: ' + typeof isCool);
+console.log('Number of Pets: ' + totalPets + ', Type: ' + typeof totalPets);
+console.log('Unknown: ' + unknown + ', Type: ' + typeof unknown);
+console.log('Never: ' + never + ', Type: ' + typeof never);
