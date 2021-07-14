e5caae7138a2318dbbfef59bec935c8632e1f30e
Sam Vivonia <snvivonia@gmail.com>
2021-07-13 19:44:06 -0700

    Completed javascript-arrays

---
+++ javascript-arrays/main.js
@@ -0,0 +1,30 @@
+// COLORS
+
+var colors = ['red', 'white', 'blue'];
+
+console.log('Colors 0: ', colors[0]);
+console.log('Colors 1: ', colors[1]);
+console.log('Colors 2: ', colors[2]);
+
+console.log('America is ' + colors[0] + ', ' + colors[1] + ', ' + colors[2] + '.');
+
+colors[2] = 'green';
+
+console.log('Mexico is ' + colors[0] + ', ' + colors[1] + ', ' + colors[2] + '.');
+
+console.log('Colors: ', colors);
+
+// STUDENTS
+
+var students = ['jack', 'jill', 'tom', 'jerry'];
+
+var numberOfStudents = students.length;
+
+console.log('Number of Students: ', numberOfStudents);
+
+var lastIndex = numberOfStudents - 1;
+
+var lastStudent = students[lastIndex];
+
+console.log('The last student in the array is: ', lastStudent);
+console.log('Students: ', students);
