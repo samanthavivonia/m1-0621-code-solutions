3dfb872d2a91f1a08bc6fc34df44d481ffbe1de8
Sam Vivonia <snvivonia@gmail.com>
2021-07-15 12:04:12 -0700

    Complete javascript-data-model

---
+++ javascript-data-model/bookshelf.js
@@ -0,0 +1,21 @@
+var bookshelf = [
+  {
+    isbn: '978-1491929483',
+    title: 'Introduction to JavaScript Object Notation: A To-the-Point Guide to JSON',
+    author: 'Lindsay Bassett'
+  },
+  {
+    isbn: '978-1484218624',
+    title: 'JSON Quick Syntax Reference',
+    author: 'Wallace Jackson'
+  },
+  {
+    isbn: '978-0692232699',
+    title: 'Build APIs You Won\'t Hate: Everyone and their dog wants an API, so you should probably learn how to build them',
+    author: 'Phil Sturgeon & Laura Bohill'
+  }
+];
+
+console.log('2nd Book Author: ', bookshelf[1].author);
+console.log('1st Book ISBN: ', bookshelf[0].isbn);
+console.log('3rd Book Title: ', bookshelf[2].title);
